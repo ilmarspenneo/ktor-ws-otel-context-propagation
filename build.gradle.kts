7ec97eef8221dbda1b4c1466c6b8bbdfec130687
Ilmārs <132436248+ilmarspenneo@users.noreply.github.com>
2025-04-11 18:00:38 +0200

    generate project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.cio.EngineMain"
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.websockets)
+    implementation(libs.ktor.server.cio)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.cio.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSockets()
+    configureRouting()
+}
